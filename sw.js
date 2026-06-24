03907da513be6cfea678ae8f34bb83991d4ab292
DeveloperTangle <polandballhub@gmail.com>
2026-06-25 07:54:36 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+var CACHE_NAME = 'mtz-editor-online';
+var urlsToCache = [
+	'/mtz-editor-online/',
+];
+
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches
+			.open(CACHE_NAME)
+			.then(function(cache) {
+				return cache.addAll(urlsToCache);
+			})
+	);
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then(function(response) {
+				return response ? response : fetch(event.request);
+			})
+	);
+});
